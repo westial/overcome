7a41194eefa77af85cd8cd783a95168f223329b8
jaume <jaume@westial.com>
2022-12-09 12:04:34 +0900

    Add stack package to setup

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='overcome',
     version='0.1.0',
-    packages=['src', 'src.overcome', 'src.overcome.position'],
-    url='',
+    packages=['src', 'src.overcome', 'src.stack', 'src.overcome.position'],
+    url='https://github.com/westial/overcome',
     license='GPLv3+',
     author='Jaume Mila Bea',
     author_email='jaume@westial.com',
-    description='Calculate the bid/ask outcome of every row in a dataframe with '
-                'stock exchange candle bars data (OHLCV).',
+    description='Calculate the potential buying and selling earnings from the '
+                '"high", "low" and "close" trading history values',
     install_requires=['numpy'],
     extras_require={
         'dev': [
